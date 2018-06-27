39b2375133c4667f46488de057975a2ef8dc663d
Jennifer <jenn.chen@live.ca>
2018-06-27 16:55:38 -0700

    Add event listeners for hovering mouse

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const ET = document.getElementById('layer5').querySelectorAll("path[inkscape\\:label='ET']") 
+  const ET = document.getElementById('layer5').querySelectorAll("path[inkscape\\:label='ET']")
+
+  ET.forEach(element => {
+    element.addEventListener('mouseenter', event => {
+      const {currentTarget} = event;
+      currentTarget.classList.add('highlight')
+    })
+
+    element.addEventListener('mouseleave', event => {
+      const {currentTarget} = event;
+      currentTarget.classList.remove('highlight')
+    })
+  })
 });
