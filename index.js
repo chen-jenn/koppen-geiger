32176a77468fe57146bec73518090223af3da0f5
Jennifer <jenn.chen@live.ca>
2018-06-29 18:22:52 -0700

    Remove some inline styling

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const ET = document.getElementById('layer5').querySelectorAll("path[inkscape\\:label='ET']")
+  const ET = document.getElementById('ET')
+  // const ET = document.getElementById('layer5').querySelectorAll("path[inkscape\\:label='ET']")
 
-  ET.forEach(element => {
-    element.addEventListener('mouseenter', event => {
-      const {currentTarget} = event;
-      currentTarget.classList.add('highlight')
-    })
+  ET.addEventListener('mouseenter', event => {
+    const {currentTarget} = event;
+    currentTarget.classList.add('highlight');
+  });
 
-    element.addEventListener('mouseleave', event => {
-      const {currentTarget} = event;
-      currentTarget.classList.remove('highlight')
-    })
-  })
+  ET.addEventListener('mouseleave', event => {
+    const {currentTarget} = event;
+    currentTarget.classList.remove('highlight');
+  });
+
 });
