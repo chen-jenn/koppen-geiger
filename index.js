f47e765598c9034d432393f0c290a106c5c02e0b
Jennifer <jenn.chen@live.ca>
2018-07-02 14:39:01 -0700

    Add d3 js file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const climateZones = document.querySelectorAll("g:not(#g2065):not(#layer2):not(#viewport-20180630013332566):not(#g1755)");
+
   // Don't really know what these random groups are, remove them later
   // const ET = document.getElementById('layer5').querySelectorAll("path[inkscape\\:label='ET']")
-
   climateZones.forEach(element => {
     element.addEventListener('mouseenter', event => {
       const {currentTarget} = event;
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const {currentTarget} = event;
       currentTarget.classList.remove('highlight');
     });
+
   })
 
   svgPanZoom(document.getElementById('svg2'));
